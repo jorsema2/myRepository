79ae543f43aad8f78c4c238507bc84a6e789fe6b
Esteban Munch Jones <49006803+estebanmunchjones2019@users.noreply.github.com>
2020-02-04 12:56:05 +0000

    Update Pythagoras.js

--- Pythagoras/Pythagoras.js
+++ Pythagoras/Pythagoras.js
@@ -1,16 +1,10 @@
-function sumSquaredCatheti(cathetus1, cathetus2) {
-    if (cathetus1 <= 0 || cathetus2 <= 0) {
-        return console.log('The catheti must have positive integer values.'); //Prints The catheti must have positive integer values.
-    }
-    return sum = Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2); //Returns the sum of the squared catheti.
-}
-
-function squareRoot(cathetus1, cathetus2) {
-    return Math.sqrt(sumSquaredCatheti(cathetus1, cathetus2)); //Returns the square root of the sum of the squared catheti.
-}
-
 function hypotenuse(cathetus1, cathetus2) {
-    return squareRoot(cathetus1, cathetus2); //Returns the value of the square root of the sum of the squared catheti, i.e. the hypotenuse.
+    if (cathetus1 <= 0 || cathetus2 <= 0) {
+        throw Error('The catheti must have positive integer values.');
+    }
+    const sum = Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2); //Returns the sum of the squared catheti.
+    const hypotenuse = Math.sqrt(sum);
+    return hypotenuse;
 }
 
 console.log(hypotenuse(2, 4)); //Example that follows the 'if' statement of the first function.
